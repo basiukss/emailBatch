b10e1f9d1f3a9e69ca186621d9f9087447d18014
Sergey Basyuk <sergey.basyuk@succraft.com>
2019-03-26 17:00:04 +0300

    add batch

--- force-app/main/default/lwc/emailBatch/emailBatch.js
+++ force-app/main/default/lwc/emailBatch/emailBatch.js
@@ -1,12 +1,15 @@
 import { LightningElement, wire, track } from 'lwc';
 //import getRecordList from '@salesforce/apex/RecordController.getRecordList';
-import getEmailConfig from '@salesforce/apex/emailConfig.getEmailConfig';
-import getPDF from '@salesforce/apex/emailConfig.getPDF';
+//import getEmailConfig from '@salesforce/apex/emailConfig.getEmailConfig';
+import runBatch from '@salesforce/apex/emailConfig.runBatch';
+import getStatus from '@salesforce/apex/emailConfig.getStatus';
 
 export default class EmailBatch extends LightningElement {
     @track fieldSet;
     @track error;
-
+    @track batchId;
+    @track batchStatus;
+/*
     @wire(getEmailConfig, {})
     getEmailConfig({ error, data }) {
         if (data) {
@@ -19,23 +22,29 @@ export default class EmailBatch extends LightningElement {
             console.log(error);
         }
     }
-
-    @wire(getPDF, {})
-    getPDF({ error, data }) {
-        if (data) {            
-            console.log('getPDF');
-            let pdfbase64 = JSON.parse(data);
-            let byteCharacters = atob(pdfbase64);
-            let byteNumbers = new Array(pdfbase64.length);
-            for (let i = 0; i < byteCharacters.length; i++) {
-                byteNumbers[i] = byteCharacters.charCodeAt(i);
-            }
-            let byteArray = new Uint8Array(byteNumbers);
-            let blob = new Blob([byteArray], {type: "application/pdf"});
-            console.log(blob);
-        } else if (error) {
-            this.error = error;
-            console.log('getPDF error');
-        }
+*/   
+    handleRunBatch() {
+        console.log('runBatch');
+        runBatch()
+            .then(data => {                
+                console.log(data);
+                this.batchId = data;
+            })
+            .catch(error => {
+                console.log('runBatch error');
+                this.error = error;
+            });
+    }
+    handleGetStatus() {
+        console.log('getStatus');
+        getStatus({jobId : this.batchId})
+            .then(data => {                
+                console.log(data);
+                this.batchStatus = data.status;
+            })
+            .catch(error => {
+                console.log('getStatus error');
+                this.error = error;
+            });
     }
 }
